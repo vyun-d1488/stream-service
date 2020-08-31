8126a705dc9f5fe13ecaa0bb9b952b3b78cdff4f
vyun-d1488 <ilyaspiypiy@gmail.com>
2020-08-31 19:58:52 +0600

    1

--- internals/webpack.client.js
+++ internals/webpack.client.js
@@ -7,28 +7,28 @@ const webpack = require("webpack");
 const rules = require("./rules");
 
 module.exports = {
-      entry: "./client/index.js",
-      output: {
-            filename: "bundle.js",
-            path: path.resolve(process.cwd(), "public"),
-      },
-      module: {
-            rules: rules,
-      },
-      devtool: false,
+	entry: "./client/index.js",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(process.cwd(), "build/public"),
+	},
+	module: {
+		rules: rules,
+	},
+	devtool: false,
 
-      plugins: [
-            new ProgressBarPlugin(),
-            new UglifyJsPlugin(),
-            new MiniCssExtractPlugin({
-                  filename: "style.css",
-            }),
-      ],
-      mode: devMode ? "development" : "production",
-      watch: devMode,
-      performance: {
-            maxAssetSize: 1000000,
-            maxEntrypointSize: 1000000,
-            hints: devMode ? "warning" : false,
-      },
+	plugins: [
+		new ProgressBarPlugin(),
+		new UglifyJsPlugin(),
+		new MiniCssExtractPlugin({
+			filename: "style.css",
+		}),
+	],
+	mode: devMode ? "development" : "production",
+	watch: devMode,
+	performance: {
+		maxAssetSize: 1000000,
+		maxEntrypointSize: 1000000,
+		hints: devMode ? "warning" : false,
+	},
 };
--- server/config/default.js
+++ server/config/default.js
@@ -4,38 +4,37 @@ const SECRET = process.env.SECRET;
 const FFMPEG = process.env.FFMPEG;
 
 const config = {
-      logType: 3,
+	logType: 3,
 
-      server: {
-            secret: SECRET,
-      },
-      rtmp_server: {
-            rtmp: {
-                  port: 1935,
-                  chunk_size: 30000,
-                  gop_cache: true,
-                  ping: 60,
-                  ping_timeout: 30,
-            },
-            http: {
-                  port: 8888,
-                  mediaroot: "./server/media",
-                  allow_origin: "*",
-            },
-            trans: {
-                  ffmpeg: FFMPEG,
-                  tasks: [
-                        {
-                              app: "live",
-                              rtmp: true,
-                              rtmpApp: "live-ac",
-                              hls: true,
-                              hlsFlags:
-                                    "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
-                        },
-                  ],
-            },
-      },
+	server: {
+		secret: SECRET,
+	},
+	rtmp_server: {
+		rtmp: {
+			port: 1935,
+			chunk_size: 30000,
+			gop_cache: true,
+			ping: 60,
+			ping_timeout: 30,
+		},
+		http: {
+			port: 8888,
+			mediaroot: "./server/media",
+			allow_origin: "*",
+		},
+		trans: {
+			ffmpeg: FFMPEG,
+			tasks: [
+				{
+					app: "live",
+					hls: true,
+					hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
+					dash: true,
+					dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
+				},
+			],
+		},
+	},
 };
 
 export default config;
